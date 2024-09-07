6a3999c4f4cd21b1003fecc739df3385813000f9
kiddersmxj <maxkiddmay@gmail.com>
2024-09-07 12:12:31 +0100

    updated default model

--- inc/config.hpp
+++ inc/config.hpp
@@ -6,7 +6,7 @@
 
 const std::string GlowStylesheet = "glowstyle";
 const std::string ChatArchiveDir = HOME + "/.local/share/dolores/chats/";
-const std::string DefaultModel = "gpt-4o";
+const std::string DefaultModel = "gpt-4o-mini";
 const int DefaultMaxTokens = 150;
 
 const std::string ShortsDir = HOME + "/.local/share/dolores/shorts/";
